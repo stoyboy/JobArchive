1751cfaabb38d07aaa5c5afc8baccc913e4d2a65
stoyboy <jocas03@hotmail.com>
2022-06-24 23:21:32 +0200

    created Footer component

---
+++ components/Footer.tsx
@@ -0,0 +1,13 @@
+import { FC } from "react"
+
+export const Footer: FC = () => {
+    return (
+        <div className='mt-auto'>
+            <footer className='footer footer-center p-4 bg-base-300 text-base-content'>
+                <div>
+                    <p>Copyright © 2022 - Jovan Stojimirovic</p>
+                </div>
+            </footer>
+        </div>
+    )
+}
