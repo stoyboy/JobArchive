98dd7cc2f8e4338dc77a6dbac8f15a05a2b3c484
stoyboy <jocas03@hotmail.com>
2022-06-26 04:57:54 +0200

    added index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,31 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
+import type { NextPage } from "next";
+import Head from "next/head";
+import { Body } from "../components/Body";
+import { Footer } from "../components/Footer";
+import { Dashboard } from "../components/Root/Dashboard";
 
 const Home: NextPage = () => {
   return (
-    <div>
+    <div
+      className="flex flex-col w-full h-screen min-h-screen"
+      data-theme="winter"
+    >
       <Head>
         <title>JobArchive</title>
-        <meta name="description" content="JobArchive is a webtool which helps you to manage your company contacts" />
+        <meta
+          name="description"
+          content="JobArchive is a webtool which helps you to manage your company contacts"
+        />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-    </div>
-  )
-}
 
-export default Home
+      <Body>
+        <Dashboard />
+      </Body>
+
+      <Footer />
+    </div>
+  );
+};
+
+export default Home;
