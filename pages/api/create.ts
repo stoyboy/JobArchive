64e8f1bb65bdf2a94237c24e609df71c5f0f5190
stoyboy <jocas03@hotmail.com>
2022-06-27 06:15:30 +0200

    added http method check

--- pages/api/create.ts
+++ pages/api/create.ts
@@ -4,18 +4,22 @@ import { prisma } from '../../prisma/client'
 
 export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Contact>
+  res: NextApiResponse<Contact | null>
 ) {
-  const response = await prisma.contact.create({
-    data: {
-      firstName: req.body.firstName,
-      lastName: req.body.lastName,
-      company: req.body.company,
-      field: req.body.field,
-      location: req.body.location,
-      date: req.body.date,
-      info: req.body.info
-    }
-  })
-  res.status(200).json(response)
+  if (req.method === 'POST') {
+    const response = await prisma.contact.create({
+      data: {
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        company: req.body.company,
+        field: req.body.field,
+        location: req.body.location,
+        date: req.body.date,
+        info: req.body.info
+      }
+    })
+    res.status(200).json(response)
+  } else {
+    res.status(405).json(null)
+  }
 }
