ba6c3a496a7eb5b121c1644ac6d4ff82623ad004
stoyboy <jocas03@hotmail.com>
2022-07-03 13:40:09 +0200

    added PageHeader in ContactView

--- components/Contact/View.tsx
+++ components/Contact/View.tsx
@@ -1,4 +1,4 @@
-import { Button, DatePicker, Form, Input } from 'antd';
+import { Button, DatePicker, Form, Input, PageHeader } from 'antd';
 import 'moment/locale/de-at';
 import locale from 'antd/lib/locale/de_DE';
 import 'antd/dist/antd.css';
@@ -28,86 +28,87 @@ export const View: FC<{ data: Contact }> = (props) => {
 
   return (
     <div className='m-6'>
-      <div className='flex flex-row justify-start items-center'>
-        <Button>
-          <Link href='/'>Zurück zum Dashboard</Link>
-        </Button>
-      </div>
-      <h1 className='text-2xl my-9'>Kontakt bearbeiten</h1>
-      <Form
-        name='create'
-        initialValues={{
-            ...props.data,
-            date: moment(props.data.date)
-        }}
-        onFinish={onFinish}
-        autoComplete='off'
-        className='grid grid-cols-2 gap-4 max-w-2xl'
+      <h1 className='text-2xl my-9'></h1>
+      <PageHeader
+        ghost={false}
+        onBack={() => Router.push("/dashboard")}
+        title='Kontakt bearbeiten'
       >
-        <Form.Item
-          name='firstName'
-          rules={[{ required: true, message: 'Pflichtfeld' }]}
+        <Form
+          name='create'
+          initialValues={{
+            ...props.data,
+            date: moment(props.data.date),
+          }}
+          onFinish={onFinish}
+          autoComplete='off'
+          className='grid grid-cols-2 gap-4 max-w-2xl'
         >
-          <Input placeholder='Vorname' />
-        </Form.Item>
-
-        <Form.Item
-          name='lastName'
-          rules={[{ required: true, message: 'Pflichtfeld' }]}
-        >
-          <Input placeholder='Nachname' />
-        </Form.Item>
-
-        <Form.Item
-          name='company'
-          rules={[{ required: true, message: 'Pflichtfeld' }]}
-          className='col-span-2'
-        >
-          <Input placeholder='Unternehmen' />
-        </Form.Item>
-
-        <Form.Item
-          name='field'
-          rules={[{ required: true, message: 'Pflichtfeld' }]}
-          className='col-span-2'
-        >
-          <Input placeholder='Position' />
-        </Form.Item>
-
-        <Form.Item
-          name='location'
-          rules={[{ required: true, message: 'Pflichtfeld' }]}
-        >
-          <Input placeholder='Erstbegegnung' />
-        </Form.Item>
-
-        <Form.Item
-          name='date'
-          rules={[{ required: true, message: 'Pflichtfeld' }]}
-        >
-          <DatePicker
-            placeholder='Datum der Erstbegegnung'
-            format={'DD/MM/YYYY'}
-            locale={locale.DatePicker}
-            className='w-full'
-          />
-        </Form.Item>
-
-        <Form.Item name='info' className='col-span-2'>
-          <TextArea placeholder='Info' />
-        </Form.Item>
-
-        <Form.Item>
-          <Button
-            type='primary'
-            htmlType='submit'
-            className='w-full'
-            loading={isLoading}
+          <Form.Item
+            name='firstName'
+            rules={[{ required: true, message: 'Pflichtfeld' }]}
           >
-            Änderungen übernehmen
-          </Button>
-        </Form.Item>
-      </Form>
+            <Input placeholder='Vorname' />
+          </Form.Item>
+
+          <Form.Item
+            name='lastName'
+            rules={[{ required: true, message: 'Pflichtfeld' }]}
+          >
+            <Input placeholder='Nachname' />
+          </Form.Item>
+
+          <Form.Item
+            name='company'
+            rules={[{ required: true, message: 'Pflichtfeld' }]}
+            className='col-span-2'
+          >
+            <Input placeholder='Unternehmen' />
+          </Form.Item>
+
+          <Form.Item
+            name='field'
+            rules={[{ required: true, message: 'Pflichtfeld' }]}
+            className='col-span-2'
+          >
+            <Input placeholder='Position' />
+          </Form.Item>
+
+          <Form.Item
+            name='location'
+            rules={[{ required: true, message: 'Pflichtfeld' }]}
+          >
+            <Input placeholder='Erstbegegnung' />
+          </Form.Item>
+
+          <Form.Item
+            name='date'
+            rules={[{ required: true, message: 'Pflichtfeld' }]}
+          >
+            <DatePicker
+              placeholder='Datum der Erstbegegnung'
+              format={'DD/MM/YYYY'}
+              locale={locale.DatePicker}
+              className='w-full'
+            />
+          </Form.Item>
+
+          <Form.Item name='info' className='col-span-2'>
+            <TextArea placeholder='Info' rows={6}/>
+          </Form.Item>
+
+          <Form.Item>
+            <Button
+              type='primary'
+              htmlType='submit'
+              className='w-full'
+              loading={isLoading}
+            >
+              Änderungen übernehmen
+            </Button>
+          </Form.Item>
+        </Form>
+      </PageHeader>
     </div>
   );
 };
