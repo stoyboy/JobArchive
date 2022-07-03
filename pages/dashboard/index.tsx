9fc262508a06948aa5b635edc712943b2d4b4ad1
stoyboy <jocas03@hotmail.com>
2022-07-03 13:33:05 +0200

    moved Dashboard site

---
+++ pages/dashboard/index.tsx
@@ -0,0 +1,50 @@
+import type { GetServerSideProps, NextPage } from "next";
+import Head from "next/head";
+import { Body } from "../../components/Body";
+import { Footer } from "../../components/Footer";
+import { Dashboard } from "../../components/Dashboard/Dashboard";
+import { auth } from "../../firebase/firebase";
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const user = auth.currentUser;
+
+  if (!user) {
+    return {
+      redirect: {
+        permanent: false,
+        destination: '/',
+      },
+      props: {},
+    };
+  } else {
+    return {
+      props: {},
+    };
+  }
+};
+
+const Home: NextPage = () => {
+  return (
+    <div
+      className="flex flex-col w-full h-screen min-h-screen"
+      data-theme="winter"
+    >
+      <Head>
+        <title>JobArchive</title>
+        <meta
+          name="description"
+          content="JobArchive is a webtool which helps you to manage your company contacts"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <Body>
+        <Dashboard />
+      </Body>
+
+      <Footer />
+    </div>
+  );
+};
+
+export default Home;
